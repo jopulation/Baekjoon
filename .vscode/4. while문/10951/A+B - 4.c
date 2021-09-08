a3c23c9763ffb9660c504e65e433fcf7c6cfbcb1
jopulation <jopulation@yonsei.ac.kr>
2021-09-08 14:59:10 +0900

    4. while문

---
+++ .vscode/4. while문/10951/A+B - 4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int A, B;
+    while(scanf("%d %d", &A, &B) != EOF) {
+        printf("%d\n", (A+B));
+    }
+    return 0;
+}
+
+// 데이터 끝나면 EOF를 이용!
+
---
+++ .vscode/4. while문/1110/더하기 사이클.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int N, M; // 0 <= N <= 99
+    int count = 0;
+    
+    scanf("%d", &N);
+    M = N;
+    do {
+        M = (M % 10)*10 + ( ( (M/10) + (M%10) )% 10 );
+        count++;
+    } while(N != M);
+
+    printf("%d", count);
+    
+    return 0;
+}
