da1c28ae8757a12ae7ac9e27f06c1472f681d2c0
jopulation <jopulation@yonsei.ac.kr>
2022-06-30 00:13:47 +0900

    2. 조건문

---
+++ .vscode/2. 조건문/2525/오븐 시계.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, C;
+    scanf("%d%d", &A, &B);
+    scanf("%d", &C);
+    A = (A+ (B+C)/60) % 24;
+    B = (B+C)%60;
+    printf("%d %d", A, B);
+    return 0;
+}
