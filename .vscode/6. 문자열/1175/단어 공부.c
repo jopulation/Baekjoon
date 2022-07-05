8de8d022e4b1f697ea8987724331427b850662b2
jopulation <jopulation@yonsei.ac.kr>
2022-07-05 17:41:58 +0900

    6-1175 외 않돌아가는지 모름...

---
+++ .vscode/6. 문자열/1175/단어 공부.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    int i, j;
+    int max;
+    int maxNum = 0;
+
+    char answer;
+    char string[1000001];
+    int alpha[26] = { 0, };
+    
+    string[0] = '\0';
+    gets(string);
+
+    for(i = 0; string[i] != '\0'; i++) {
+        // 소문자를 대문자로 바꾸기
+        if(islower(string[i]))
+            string[i] = toupper(string[i]);
+
+        for(j = 0; j < 26; j++) {
+            if(string[i] == 'A' + j) {
+                alpha[j]++;
+            }
+        }
+    }
+
+    // Max 찾기
+    max = alpha[0];
+
+    for(i = 0; i < 26; i++) {
+        if(max < alpha[i]) {
+            max = alpha[i];
+            maxNum = i;
+        }
+    }
+
+    // 최댓값이 중복인지 찾기
+    for(i = 0; i < 26; i++) {
+        if(i != maxNum){
+            if(max == alpha[i]) {
+                answer = '?';
+                break;
+            }
+        }
+    }
+
+    if(answer != '?')
+        answer = 'A' + maxNum;
+
+    printf("%c", answer);
+
+    return 0;
+}
