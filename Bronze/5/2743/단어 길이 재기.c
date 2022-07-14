4e67ad511bb55bbdcae08693c0ec3d0bb00e5c1b
jopulation <jopulation@yonsei.ac.kr>
2022-07-14 23:21:34 +0900

    Bronze_5

---
+++ Bronze/5/2743/단어 길이 재기.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char words[101];
+
+    scanf("%s", &words);
+
+    printf("%d", strlen(words));
+    
+    return 0;
+}
---
+++ Bronze/5/2744/대소문자 바꾸기.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int i;
+    int len;
+    char words[101];
+
+    gets(words);
+
+    len = strlen(words);
+    for(i = 0; i < len; i++) {
+        // 대문자
+        if(words[i] >= 65 && words[i] <= 90) {
+            words[i] = words[i] - 'A' + 'a';
+        }
+        else if(words[i] >= 97 && words[i] <= 122) {
+            words[i] = words[i] - 'a' + 'A';
+        }
+    }
+
+    puts(words);
+    return 0;
+}
