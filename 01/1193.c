061a293cd4f3c981fb99d2600ffe7f1e9e321c50
jopulation <jopulation@yonsei.ac.kr>
2022-11-19 01:22:29 +0900

    1193 개선

--- 01/1193.c
+++ 01/1193.c
@@ -2,51 +2,36 @@
 #include <stdio.h>
 
 int numSearch(int n);
-int numSum(int n);
 
 int main()
 {
     int X; // 1 ≤ X ≤ 10,000,000
-    int num;
     int row, col;
     int n;
     int sum;
+    int tmp;
 
-    scanf("%d", &num);
-    n = numSearch(num);
-    sum = numSum(num);
+    scanf("%d", &X);
+    n = numSearch(X);
+    sum = n * (n + 1) / 2;
 
+    row = n;
+    col = 1;
+    if (X != sum)
+    {
+        row++;
+        sum++;
+        row -= (X - sum);
+        col += (X - sum);
+    }
     if (n % 2 == 0)
     {
-        row = 1;
-        col = n;
-        if (num != sum)
-        {
-            col++;
-            sum++;
-        }
-        for (sum; sum < num; sum++)
-        {
-            row++;
-            col--;
-        }
-    }
-    else
-    {
-        row = n;
-        col = 1;
-        if (num != sum)
-        {
-            row++;
-            sum++;
-        }
-        for (sum; sum < num; sum++)
-        {
-            row--;
-            col++;
-        }
+        tmp = row;
+        row = col;
+        col = tmp;
     }
     printf("%d/%d\n", col, row);
+
     return 0;
 }
 
@@ -59,19 +44,6 @@ int numSearch(int n)
         num -= i;
     }
     return i - 1;
-}
-
-int numSum(int n)
-{
-    int i;
-    int num = n;
-    int sum = 0;
-    for (i = 1; num - i >= 0; i++)
-    {
-        num -= i;
-        sum += i;
-    }
-    return sum;
 }
 // 규칙 찾기
 /*
