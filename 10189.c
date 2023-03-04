698dfe3da437609fa71247e1829242172faf0bed
jopulation <83256159+jopulation@users.noreply.github.com>
2023-03-04 20:24:53 +0900

    10189

---
+++ 10189.c
@@ -0,0 +1,12 @@
+// Hook
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	printf("#  # #### #### #  #\n");
+	printf("#### #  # #  # # #\n");
+	printf("#### #  # #  # # #\n");
+	printf("#  # #### #### #  #\n");
+
+	return 0;
+}
