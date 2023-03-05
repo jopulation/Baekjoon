265ad97893eddf1b2720d209576bec78921894ea
jopulation <83256159+jopulation@users.noreply.github.com>
2023-03-05 18:02:51 +0900

    21598

---
+++ 21598.c
@@ -0,0 +1,15 @@
+// SciComLove
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int N; // 1 <= N <= 2
+
+	scanf("%d", &N);
+	if (N == 1)
+		printf("SciComLove\n");
+	else if(N==2)
+		printf("SciComLove\nSciComLove\n");
+
+	return 0;
+}
