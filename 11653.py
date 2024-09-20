f4195698d4916a0c6842822ad4e98d8f823c81dc
jopulation <83256159+jopulation@users.noreply.github.com>
2024-09-20 16:48:57 +0900

    11653 - 수정

--- 11653.py
+++ 11653.py
@@ -1,34 +1,21 @@
 # 소인수분해
 
-# 소수 판별
-def PrimeCheck(N):
-    primeList = [2]
-    nextNum = 3 # 두 번째 소수부터 시작
-    num = N
-    cnt = 0
+def prime(n):
+  while n%2 == 0:
+    print(2)
+    n /= 2
+  
+  d = 3
+  while d*d <= n:
+    while n%d == 0:
+      print(d)
+      n /= d
+    d += 1
+  
+  if n != 1:
+    print(int(n))
 
-    while num % 2 == 0:
-        num /= 2
-        print(2)
-        cnt += 1
+n  = int(input())
 
-    while nextNum <= num + 1:
-        for p in primeList:
-            if p > nextNum / p:
-                primeList.append(nextNum)
-                while num % nextNum == 0:
-                    num /= nextNum
-                    print(nextNum)
-                    cnt += 1
-                break
-
-            elif nextNum % p == 0:
-                break
-        nextNum += 1
-
-    if cnt == 0: # N이 소수일 때
-        print(N)
-
-#  1<= N <= 10,000,000
-N = int(input())
-PrimeCheck(N)
+if n != 1:
+  prime(n)
