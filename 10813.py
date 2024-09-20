7e880f939d3e0291ef8c898aa7e6804e239bd170
jopulation <83256159+jopulation@users.noreply.github.com>
2024-09-20 23:36:11 +0900

    10813 - 수정
    range를 사용한 리스트
    파이썬의 swap

--- 10813.py
+++ 10813.py
@@ -1,19 +1,10 @@
 # 공 바꾸기
 
-def swap(arr, a, b):
-    tmp = arr[a-1]
-    arr[a-1] = arr[b-1]
-    arr[b-1] = tmp
-
-
 N, M = map(int, input().split())
-basket = [0]*N
-
-for i in range(N):
-    basket[i] = i+1
+basket = list(range(N+1))
 
 for a in range(M):
     i, j = map(int, input().split())
-    swap(basket, i, j)
+    basket[i], basket[j] = basket[j], basket[i]
 
-print(*basket)
+print(*basket[1:])
