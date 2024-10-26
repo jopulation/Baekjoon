7fc694d276f2f4aa3fb64941c59c8a39d5bf1e38
jopulation <83256159+jopulation@users.noreply.github.com>
2024-10-26 10:56:34 +0900

    24264 - 알고리즘 수업 - 알고리즘의 수행 시간 3
    cnt 삭제
    sum += A[i]*A[j] 수정

--- 24264.py
+++ 24264.py
@@ -1,10 +1,10 @@
 # 알고리즘 수업 - 알고리즘의 수행 시간 3
 
-def ManOfPassion(A, n, cnt):
+def ManOfPassion(A, n):
     sum = 0
     for i in range(1, n+1):
         for j in range(i, n+1):
-            sum += A[i] # 코드1
+            sum += A[i]*A[j] # 코드1
     return sum
 
 n = int(input())
