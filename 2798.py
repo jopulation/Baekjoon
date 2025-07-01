1e0d55326aff6c50d47c7d4091a984179aeb9fec
jopulation <83256159+jopulation@users.noreply.github.com>
2025-07-01 05:19:34 +0000

    2798 - 블랙잭

---
+++ 2798.py
@@ -0,0 +1,20 @@
+# 블랙잭
+
+n, m = map(int, input().split()) # N(3 ≤ N ≤ 100), M(10 ≤ M ≤ 300,000)
+cards = map(int, input().split())
+cards = list(cards)
+# print(*cards)
+h = 0
+
+for i in range(0, n-2):
+    sum1 = cards[i]
+    for j in range(i+1, n-1):
+        sum2 = sum1+cards[j]
+        for k in range(j+1, n):
+            sum = sum2 + cards[k]
+            if(sum == m):
+                print(m)
+                exit(0)
+            elif(sum>h and sum <m):
+                h = sum
+print(h)
